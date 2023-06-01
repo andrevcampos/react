845f5fdcaefd5d9f66704db43b13d9171560cc09
Andre Campos <andrevcampos@hotmail.com>
2023-06-01 16:21:25 +1200

    updates

--- reactProject/App.js
+++ reactProject/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app2</Text>
       <StatusBar style="auto" />
+
+
+      <Text>--------------------------------------------</Text>
+      <Text>How to make image pressable??</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://oceaniaesports.gg/wp-content/uploads/2021/07/logowebsite.png',
+          width:100,
+          height:100,
+        }}
+      />
     </View>
+    
   );
 }
 
